a50cd8ef5d23148afe50d76c60353604a2fbbda4
Amit Solanki <amit.fash@gmail.com>
2015-03-02 19:56:51 +0530

    re ordering

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,22 +1,22 @@
 class ReportsController < ApplicationController
   def index
-    @labour = Labour.all
+    @labour = Labour.all.order(:first_name)
   end
   def long
     @labours = Labour.all
     if params[:labour]
       @labour = Labour.find(params[:labour][:id])
     else
-      @labour = Labour.all.first
+      @labour = Labour.all.order(:first_name).first
     end
   end
   def client
-    @clients = Client.all
-    @labours = Labour.all
+    @clients = Client.all.order(:first_name)
+    @labours = Labour.all.order(:first_name)
     if params[:client]
       @client = Client.find(params[:client][:id])
     else
-      @client = Client.all.first
+      @client = Client.all.order(:first_name).first
     end
   end
 end
