560a00e752284f4b17a30de81b84f6afbbf7d280
Amit Solanki <amit.fash@gmail.com>
2015-03-29 23:54:38 +0530

    ..

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,22 +1,22 @@
 class ReportsController < ApplicationController
   def index
-    @labour = Labour.all.order(:first_name)
+    @labour = Labour.where("labours.on_vacation is not true").order(:first_name)
   end
   def long
-    @labours = Labour.all
+    @labours = Labour.where("labours.on_vacation is not true")
     if params[:labour]
       @labour = Labour.find(params[:labour][:id])
     else
-      @labour = Labour.all.order(:first_name).first
+      @labour = Labour.where("labours.on_vacation is not true").order(:first_name).first
     end
   end
   def client
-    @clients = Client.all.order(:first_name)
-    @labours = Labour.all.order(:first_name)
+    @clients = Client.where("clients.on_vacation is not true").order(:first_name)
+    @labours = Labour.where("labours.on_vacation is not true").order(:first_name)
     if params[:client]
       @client = Client.find(params[:client][:id])
     else
-      @client = Client.all.order(:first_name).first
+      @client = Client.where("clients.on_vacation is not true").order(:first_name).first
     end
   end
 end
