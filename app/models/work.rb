3e595ad05ac89b768760c30187473f74b5311c8a
Yuvraj Chouhan <dilip.chouhan7@gmail.com>
2015-09-01 19:14:24 -0700

    ..

--- app/models/work.rb
+++ app/models/work.rb
@@ -9,4 +9,7 @@ class Work < ActiveRecord::Base
   def name_with_client_name
     "#{client.first_name} #{client.last_name} - #{self.name}"
   end
+
+  scope :client, -> {joins(:client)}
+  scope :client_not_on_vacation, -> {client.where("clients.on_vacation is not true")}
 end
